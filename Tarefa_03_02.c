fc1e4ce5ea6a8c746be52eeaf879c60de79bb30a
Gabriel Oliveira <gab.costa2236@gmail.com>
2025-02-09 17:43:58 -0300

    Adicionadas as funções finais do programa

--- Tarefa_03_02.c
+++ Tarefa_03_02.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
 
-// BIBLIOTECAS PARA A MATRIZ LED
+
 #include <math.h>
 #include "hardware/pio.h"
 #include "hardware/clocks.h"
 #include "hardware/adc.h"
 #include "pico/bootrom.h"
+#include "hardware/i2c.h"
+
 
 #include "Tarefa_03_02.pio.h"
+#include "inc/ssd1306.h"
+#include "inc/font.h"
+
+#define I2C_PORT i2c1
+#define I2C_SDA 14
+#define I2C_SCL 15
+#define endereco 0x3C
+ssd1306_t ssd;
 
 // Definições da matriz de LEDs
 #define NUM_PIXELS 25
@@ -30,12 +40,45 @@ void gpio_irq_handler(uint gpio, uint32_t events) {
 
     if (current_time - last_time > 300000) {
         if (gpio == BOTTON_A ) {
+
+            
             
             gpio_put(led_g, !gpio_get(led_g));
+            if(gpio_get(led_g) == true){
+            printf("LED verde ligado\n");
+            ssd1306_fill(&ssd, false); // Limpa o display
+            ssd1306_draw_string(&ssd, "LED G on", 10, 30); // Escreve no display (posição X=10, Y=30)
+            ssd1306_send_data(&ssd);
+
+            }
+            else{
+            printf("LED verde desligado\n");
+
+            ssd1306_fill(&ssd, false); // Limpa o display
+            ssd1306_draw_string(&ssd, "LED G off", 10, 30); // Escreve no display (posição X=10, Y=30)
+            ssd1306_send_data(&ssd);
+
+            }
+
 
         } else if (gpio == BOTTON_B) {
 
             gpio_put(led_b, !gpio_get(led_b));
+            if(gpio_get(led_b) == true){
+            printf("LED azul ligado\n");
+            ssd1306_fill(&ssd, false); // Limpa o display
+            ssd1306_draw_string(&ssd, "LED B on", 10, 30); // Escreve no display (posição X=10, Y=30)
+            ssd1306_send_data(&ssd);
+
+            }
+            else{
+            printf("LED azul desligado\n");
+
+            ssd1306_fill(&ssd, false); // Limpa o display
+            ssd1306_draw_string(&ssd, "LED B off", 10, 30); // Escreve no display (posição X=10, Y=30)
+            ssd1306_send_data(&ssd);
+
+            }
            
         }
 
@@ -154,9 +197,32 @@ double num9[25] = {
     0.0, 0.1, 0.0, 0.1, 0.0,
     0.0, 0.1, 0.1, 0.1, 0.0};
 
+
+
+    
 int main()
 {
     stdio_init_all();
+
+    printf("Iniciando a comunicação serial...\n");
+    fflush(stdout);
+
+    i2c_init(I2C_PORT, 400 * 1000);
+
+    gpio_set_function(I2C_SDA, GPIO_FUNC_I2C); // Define a função GPIO para I2C
+    gpio_set_function(I2C_SCL, GPIO_FUNC_I2C); // Define a função GPIO para I2C
+    gpio_pull_up(I2C_SDA); // Puxa a linha de dados
+    gpio_pull_up(I2C_SCL); // Puxa a linha de relógio
+
+     // Inicializa a estrutura do display
+    ssd1306_init(&ssd, WIDTH, HEIGHT, false, endereco, I2C_PORT); // Inicializa o display
+    ssd1306_config(&ssd); // Configura o display
+    ssd1306_send_data(&ssd); // Envia os dados para o display
+
+    // Limpa o display. O display inicia com todos os pixels apagados.
+    ssd1306_fill(&ssd, false);
+    ssd1306_send_data(&ssd);
+
 
     gpio_init(led_b);
     gpio_set_dir(led_b, OUT_PIN);
@@ -186,9 +252,71 @@ int main()
     gpio_set_irq_enabled_with_callback(BOTTON_A, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
     gpio_set_irq_enabled_with_callback(BOTTON_B, GPIO_IRQ_EDGE_FALL, true, &gpio_irq_handler);
 
+     // Buffer para armazenar a string digitada
 
-    while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+while (true) {
+
+    if(stdio_usb_connected()){
+    printf("Digite algo: ");
+    
+    char c;
+
+    if (scanf("%c", &c) == 1){
+    
+    // Lê a entrada do monitor serial (limite de 29 caracteres para evitar overflow)
+
+    ssd1306_fill(&ssd, false); // Limpa o display
+    ssd1306_draw_char(&ssd, c, 10, 30); // Escreve no display (posição X=10, Y=30)
+    ssd1306_send_data(&ssd); // Atualiza o display
+
+    switch (c)
+        {
+        case '0':
+           desenho_pio(num0, pio, sm, r, g, b); 
+            break;
+
+        case '1':
+           desenho_pio(num1, pio, sm, r, g, b); 
+            break;
+
+        case '2':
+           desenho_pio(num2, pio, sm, r, g, b); 
+            break;
+            
+        case '3':
+           desenho_pio(num3, pio, sm, r, g, b); 
+            break; 
+
+        case '4':
+           desenho_pio(num4, pio, sm, r, g, b); 
+            break;
+
+        case '5':
+           desenho_pio(num5, pio, sm, r, g, b); 
+            break;
+        case '6':
+           desenho_pio(num6, pio, sm, r, g, b); 
+            break;
+            
+        case '7':
+           desenho_pio(num7, pio, sm, r, g, b); 
+            break;
+
+        case '8':
+           desenho_pio(num8, pio, sm, r, g, b); 
+            break;
+
+        case '9':
+           desenho_pio(num9, pio, sm, r, g, b); 
+            break;   
+
+        default:
+            break;
+        }
+
     }
+
+    }
+    sleep_ms(1000);
+}
 }
